d9a26a9e7633646711dd0f2a0829e5c05941cea3
Nikolaev Danil <73169453+Nicola-devJs@users.noreply.github.com>
2022-02-09 13:32:28 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+// import { LinkComponent } from "./LinkComponent";
+// import { FormCompomemt } from "./FormCompomemt";
+// import { TextComponent } from "./TextComponent";
+import { SemanticComponent } from "./SemanticComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <TextComponent />
+      <LinkComponent />
+      <FormCompomemt /> */}
+      {/* <LinkComponent /> */}
+      <SemanticComponent />
+    </div>
+  );
+}
